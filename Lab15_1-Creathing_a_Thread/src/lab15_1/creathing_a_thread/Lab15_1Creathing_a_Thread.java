4942d36524f67a669818d45c7e2be833d06b3385
GreizelSension <greizelsension@gmail.com>
2024-10-05 17:07:22 -0500

    Iniciando proyecto

---
+++ Lab15_1-Creathing_a_Thread/src/lab15_1/creathing_a_thread/Lab15_1Creathing_a_Thread.java
@@ -0,0 +1,12 @@
+
+package lab15_1.creathing_a_thread;
+
+
+public class Lab15_1Creathing_a_Thread {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola grei");
+    }
+    
+}
