5dec05e11bc0173119304cd90dcec93e76495521
GreizelSension <greizelsension@gmail.com>
2024-10-06 16:54:15 -0500

    Iniciando proyecto de cars

---
+++ Lab15_2-Simulating_a_Car_Race/src/lab15_2/simulating_a_car_race/Lab15_2Simulating_a_Car_Race.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab15_2.simulating_a_car_race;
+
+/**
+ *
+ * @author USER
+ */
+public class Lab15_2Simulating_a_Car_Race {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola gay");
+    }
+    
+}
